05b51f66689969712561c76dc553be52f0be184b
Jv Borges <borges.jvdo@gmail.com>
2022-02-28 20:39:34 -0300

    Commit Inicial da API

---
+++ src/app.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  getObject(): any {
+    return {
+      nome: 'João Victor',
+    };
+  }
+}
